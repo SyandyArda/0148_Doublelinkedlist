6f938e1b3926eeba1019c655321f4746fc01fdfd
SyandyArda <syandi.2901@gmail.com>
2024-05-04 18:35:36 +0700

    if

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -58,5 +58,18 @@ void addNode() {
 		Node* previous = NULL;
 
 		while (current != NULL && current->noMhs < newNode->noMhs)
+		{
+			previous = current;
+			current = current->next;
+		}
+		newNode->next = current;
+		newNode->prev = previous;
+
+		if (current != NULL)
+		{
+			current->prev = newNode;
+		}
+
+
 
 
