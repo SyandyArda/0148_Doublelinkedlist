bef1079e692a8f9645a95fd5d83afb847fbb8369
SyandyArda <syandi.2901@gmail.com>
2024-05-04 18:42:30 +0700

    void retraverse

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -164,6 +164,24 @@ bool search(int rollNo, Node** previous, Node** current)
 		}
 	}
 
+	void revtraverse()
+	{
+		if (listEmpty())
+			cout << "\nList is empty" << endl;
+		else
+		{
+			cout << "\nRecord in descending order of roll number are:" << endl;
+			Node* currentNode = START;
+			while (currentNode->next != NULL)
+				currentNode = currentNode->next;
+
+			while (currentNode != NULL)
+			{
+				cout << currentNode->noMhs << " " << currentNode->name << endl;
+				currentNode = currentNode->prev;
+			}
+		}
+	}
 
 
 
