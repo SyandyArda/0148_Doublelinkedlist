f85d18cc0cb1c59b6623c8c696f61b10e3da3657
SyandyArda <syandi.2901@gmail.com>
2024-05-04 18:39:09 +0700

    if start

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -99,6 +99,12 @@ bool search(int rollNo, Node** previous, Node** current)
 			cout << "\nEnter the roll number of the student whose record is to be deleted: ";
 			cin >> rollNo;
 
+			if (START == NULL)
+			{
+				cout << "List is empty" << endl;
+				return;
+			}
+
 
 
 
