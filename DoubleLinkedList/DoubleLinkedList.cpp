0c8e40f3177efcf1950139d8d18460fd47029b1f
SyandyArda <syandi.2901@gmail.com>
2024-05-04 18:43:46 +0700

    cout 1-3

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -211,6 +211,9 @@ bool search(int rollNo, Node** previous, Node** current)
 			try
 			{
 				cout << "\nMenu" << endl;
+				cout << "1. Add a record to the list" << endl;
+				cout << "2. Delete a record from the list" << endl;
+				cout << "3. View all records in the ascending order of roll numbers" << endl;
 
 
 
