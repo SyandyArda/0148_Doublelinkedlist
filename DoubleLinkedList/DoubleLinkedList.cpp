43f8ce9f8dc22fadc8de93e44df86790694e402b
SyandyArda <syandi.2901@gmail.com>
2024-05-04 18:41:10 +0700

    bool list empty

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -142,6 +142,12 @@ bool search(int rollNo, Node** previous, Node** current)
 		cout << "\x1b[32mRecord with roll number " << rollNo << " delete\x1b[0m" << endl;
 	}
 
+	bool listEmpty()
+	{
+		return (START == NULL);
+	}
+
+
 
 
 
